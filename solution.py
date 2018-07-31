25b0078bacbccdf3c90448c8e245e1a3bec86d70
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-07-31 10:04:31 -0500

    completed Mtable, DL solution, added notes. Uploaded

---
+++ solution.py
@@ -0,0 +1,10 @@
+print ' x 1 2 3 4 5 6 7 8 9 10 11 12'
+for row in range(1, 13): 
+    display_string  = ''
+    for column in range(0, 13):
+        if column is 0:
+            display_string += ' ' + str(row)
+        else:    
+            display_string += ' ' + str(column * row)
+    print display_string    
+#python solution.py
---
+++ table.py
@@ -0,0 +1,9 @@
+# python table.py
+print 'x','1','2','3','4','5','6','7','8','9','10','11','12'
+for value in range(0,12):
+    value += 1
+    print value, value*2, value*3, value*4, value*5, value*6, value*7, value*8, value*9, value*10, value*11, value*12
+    
+        
+
+
